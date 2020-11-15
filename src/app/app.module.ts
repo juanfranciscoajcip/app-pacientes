6390d691070656310ee9c74ed82220cd3dc2b4b1
Juan Francisco Ajcip <juanfranciscoajcip@gmail.com>
2020-11-15 10:45:50 -0600

    Angular Material Navbar/Toolbar | Angular Routing
    https://www.youtube.com/watch?v=ADEZTrIxj1k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//- Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -9,18 +14,27 @@ import { AppComponent } from './app.component';
 import { PacientesComponent } from './pages/pacientes/pacientes.component';
 import { ConsultasComponent } from './pages/consultas/consultas.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PacientesComponent,
     ConsultasComponent,
-    Page404Component
+    Page404Component,
+    NavbarComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
