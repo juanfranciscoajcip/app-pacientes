f9cfbffc67df48db04fbd8f1806097fd370cbe53
Juan Francisco Ajcip <juanfranciscoajcip@gmail.com>
2020-11-19 01:18:19 -0600

    Ruta Dinora a Main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MedicosComponent } from './pages/medicos/medicos.component';
 import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import {​​ PacienteComponent }​​ from './pages/paciente/paciente.component';
+import {​​ EspecialidadComponent }​​ from './pages/especialidad/especialidad.component';
+import {​​ RegistroPacienteComponent}​​ from './pages/registro-paciente/registro-paciente.component';
+import {​​ RegistroEspecialidadComponent}​​ from './pages/registro-especialidad/registro-especialidad.component';
 
 const routes: Routes = [
   {path: '', component: AppComponent, pathMatch: 'full'},
   {path: 'medico', component: MedicosComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'pacientes', component: PacientesComponent}
+  {path: 'paciente', component: PacienteComponent},
+  {path: 'pacientes', component: PacientesComponent},
+  {​​path: 'especialidades', component: EspecialidadComponent}​​,
+  {​​path: 'registroEspecialidad', component: RegistroEspecialidadComponent}​​,
+  {​​path: 'registroPaciente', component: RegistroPacienteComponent}  
 ];
 
 @NgModule({
