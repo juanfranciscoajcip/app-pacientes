799fcff79d2a9eb080b4e97b9a3318e185cf8a64
Juan Francisco Ajcip <juanfranciscoajcip@gmail.com>
2020-11-19 02:01:21 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {​​ FormsModule }​​ from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { InsertarExamenComponent } from './pages/insertar-examen/insertar-examen
     HttpClientModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
